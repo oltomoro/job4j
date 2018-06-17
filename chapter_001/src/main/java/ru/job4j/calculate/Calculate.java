dbf5464ac4d153b5c3b6c94f9dc1811f74643a0a
ism.vladimir <ism.vladimir@gmail.com>
2018-06-17 15:30:32 +0300

    1.3. JavDoc. Code convetion [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author Ism Vladimir (is.vladimir@testmail.com)
+*/
 public class Calculate{
+	/*
+	* Main
+	* @param args - arguments
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ism Vladimir (mailto:is.vladimir@testmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
