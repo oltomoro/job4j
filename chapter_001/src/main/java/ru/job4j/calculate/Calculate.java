4c18b1e8099f0c0a231fc2d4bb9b7c04ce725012
ism.vladimir <ism.vladimir>
2018-06-25 19:09:34 +0300

    Подогнал под валидацию Checkstyle

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,25 +1,27 @@
 package ru.job4j.calculate;
 
 /**
-* Calculate
-*
-* @author Ism Vladimir (is.vladimir@testmail.com)
-*/
+ * Calculate
+ *
+ * @author Ism Vladimir (is.vladimir@testmail.com)
+ */
 public class Calculate {
-	/**
-	* Main
-	* @param args - arguments
-	*/
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-	
-	/**
-	* Method echo
-	* @param name Your name
-	* @return Echo plus your name
-	*/
-	public String echo(String name) {
-		return "Echo, echo, echo: " + name;
-	}
+    /**
+     * Main
+     *
+     * @param args - arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    /**
+     * Method echo
+     *
+     * @param name Your name
+     * @return Echo plus your name
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo: " + name;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -2,11 +2,12 @@ package ru.job4j.calculator;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 public class CalculatorTest {
     @Test
-    public void whenAdd1Plus1Then2(){
+    public void whenAdd1Plus1Then2() {
         Calculator calc = new Calculator();
         calc.add(1D, 1D);
         double result = calc.getResult();
@@ -15,27 +16,27 @@ public class CalculatorTest {
     }
 
     @Test
-    public void whenSubtract1Minus1Then0(){
+    public void whenSubtract1Minus1Then0() {
         Calculator calc = new Calculator();
-        calc.subtract(1D,1D);
+        calc.subtract(1D, 1D);
         double result = calc.getResult();
         double expected = 0D;
         assertThat(result, is(expected));
     }
 
     @Test
-    public void whenDiv2Minus2Then1(){
+    public void whenDiv2Minus2Then1() {
         Calculator calc = new Calculator();
-        calc.div(2D,2D);
+        calc.div(2D, 2D);
         double result = calc.getResult();
         double expected = 1D;
         assertThat(result, is(expected));
     }
 
     @Test
-    public void whenMultiple2On2Then4(){
+    public void whenMultiple2On2Then4() {
         Calculator calc = new Calculator();
-        calc.multiple(2D,2D);
+        calc.multiple(2D, 2D);
         double result = calc.getResult();
         double expected = 4D;
         assertThat(result, is(expected));
