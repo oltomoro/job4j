ca56b31bc54e8a50b66208bcce426e66feb6a96a
ism.vladimir <ism.vladimir>
2018-07-02 13:39:46 +0300

    подогнал под Checkstyle

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -4,18 +4,18 @@ public class Point {
     private int x;
     private int y;
 
-    public Point(int x, int y){
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public double distanceTo(Point that){
+    public double distanceTo(Point that) {
         return Math.sqrt(Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y, 2));
     }
 
-    public static void main(String[] args){
-        Point a = new Point(0,1);
-        Point b = new Point(2,5);
+    public static void main(String[] args) {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
 
         double result = a.distanceTo(b);
 
