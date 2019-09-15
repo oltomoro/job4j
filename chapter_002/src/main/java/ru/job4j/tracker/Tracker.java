b554a649147e3b90bda81505bb775ddb70bb80ed
Vladimir <test>
2019-09-15 11:15:54 +0300

    2. Реализовать класс Tracker [#396] (поиск заявки через предикат)

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
+import java.util.function.Predicate;
 
 /**
  * @author ism.vladimir
@@ -27,6 +28,7 @@ public class Tracker {
 
     /**
      * Добавляет новую заявку в массив заявок
+     *
      * @param item добавляемая заявка
      * @return заявка
      */
@@ -42,21 +44,18 @@ public class Tracker {
 
     /**
      * Заменяет в массиве заявку на новую. Заменяемую заявку ищет по id
-     * @param id id заявки которую которую нужно заменить
-     * @param item новая заявка которой нужно заменить предыдущую
+     *
+     * @param id       id заявки которую которую нужно заменить
+     * @param nextItem новая заявка которой нужно заменить предыдущую
      * @return результат выполнения true/false
      */
-    public boolean replace(String id, Item item) {
+    public boolean replace(String id, Item nextItem) {
         boolean result = false;
-        if (item == null || id == null) return result;
-
-        for (int i = 0; i < this.items.length; i++) {
-            if (this.items[i] != null && this.items[i].getId().equals(id)) {
-                item.setId(id);
-                this.items[i] = item;
-                result = true;
-                break;
-            }
+        int itemIndex = getItemIndex(item -> item != null && item.getId().equals(id));
+        if (nextItem != null && id != null && itemIndex != -1) {
+            nextItem.setId(id);
+            this.items[itemIndex] = nextItem;
+            result = true;
         }
         return result;
     }
@@ -69,6 +68,7 @@ public class Tracker {
 //                for (int j = i; j < this.items.length - 1; j++) {
 //                    this.items[j] = this.items[j + 1];
 //                }
+//                counter--;
 //                result = true;
 //            }
 //        }
@@ -77,24 +77,22 @@ public class Tracker {
 
     /**
      * Удаляет по id элемент из массива и уменьшает счетчик количества элементов на 1
+     *
      * @param id - id удаляемого элемента
      * @return результат выполнения true/false
      */
     public boolean delete(String id) {
         boolean result = false;
         if (id == null) return result;
+        int itemIndex = getItemIndex(item -> item != null && item.getId().equals(id));
+        if (itemIndex != -1){
+            Item[] itemsCopy = new Item[this.items.length];
+            System.arraycopy(this.items, 0, itemsCopy, 0, itemIndex);
+            System.arraycopy(this.items, itemIndex + 1, itemsCopy, itemIndex, this.items.length - itemIndex - 1);
+            this.items = itemsCopy;
+            counter--;
 
-        for (int i = 0; i < this.items.length; i++) {
-            if (this.items[i] != null && this.items[i].getId().equals(id)) {
-                Item[] itemsCopy = new Item[this.items.length];
-                System.arraycopy(this.items, 0, itemsCopy, 0, i);
-                System.arraycopy(this.items, i + 1, itemsCopy, i, this.items.length - i - 1);
-                this.items = itemsCopy;
-                counter--;
-
-                result = true;
-                break;
-            }
+            result = true;
         }
         return result;
     }
@@ -113,6 +111,7 @@ public class Tracker {
 
     /**
      * Находит все заявки в массиве
+     *
      * @return массив найденных заявок
      */
     public Item[] findAll() {
@@ -123,6 +122,7 @@ public class Tracker {
 
     /**
      * Находит в массиве все заявки с указанным именем
+     *
      * @param name - имя искомой заявки
      * @return массив найденных заявок
      */
@@ -135,36 +135,66 @@ public class Tracker {
 
     /**
      * Находит в массиве заявку по id
+     *
      * @param id - id искомой заявки
      * @return объект найденной заявки, если ничего не найдено, то null
      */
+    //todo проверить, что item ссылается на тот же объект
     public Item findById(String id) {
-        Item item = null;
-        if (id == null) return item;
-
-        for (int i = 0; i < this.items.length; i++) {
-            if (this.items[i] != null && this.items[i].getId().equals(id)) {
-                item = this.items[i];
-                break;
-            }
-        }
-        return item;
+        Item item = getItem(i -> i != null && i.getId().equals(id));
+        return id != null && item != null ? item : null;
     }
 
+//    public Item findById(String id) {
+//        Item item = null;
+//        if (id == null) return item;
+//
+//        for (int i = 0; i < this.items.length; i++) {
+//            if (this.items[i] != null && this.items[i].getId().equals(id)) {
+//                item = this.items[i];
+//                break;
+//            }
+//        }
+//        return item;
+//    }
+
+    /**
+     * Генерирует случайный id
+     * @return сгенерированный id
+     */
     private String generateId() {
         Random rand = new Random();
         return String.valueOf(System.currentTimeMillis() + rand.nextInt());
     }
 
-    // попробовать использовать предикат
-    // только сначала написать тесты на все методы без предиката
-    // и сначала ЗАКОММИТИТЬ версию без предиката
-    // предиткат можно сделать стримом
-//    private int getItemIndex(Item item, Predicate predicate){
-//        for (int i = 0; i<items.length; i++){
-//
-//        }
-//    }
+    /**
+     * Находит заявку по заданному условию
+     * @param predicate условие по которому происходит поиск
+     * @return ссылка на объект Item найденной заяки
+     */
+    private Item getItem(Predicate<Item> predicate) {
+        Item item = null;
+        for (Item it : this.items) {
+            if (predicate.test(it)) {
+                item = it;
+            }
+        }
+        return item;
+    }
 
+    /**
+     * Находит id заявки по заданному условию
+     * @param predicate условие по которому происходит поиск
+     * @return id найденной заявки
+     */
+    private int getItemIndex(Predicate<Item> predicate) {
+        int itemIndex = -1;
+        for (int i = 0; i < this.items.length; i++) {
+            if (predicate.test(this.items[i])) {
+                itemIndex = i;
+            }
+        }
+        return itemIndex;
+    }
 
 }
